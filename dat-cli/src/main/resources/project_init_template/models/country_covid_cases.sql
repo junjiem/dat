78d8ac5421e8d57f34c479a508c04362820d58c5
Junjie.M <118170653@qq.com>
2025-10-27 10:46:16 +0800

    chore: update project init template (#102)

--- dat-cli/src/main/resources/project_init_template/models/country_covid_cases.sql
+++ dat-cli/src/main/resources/project_init_template/models/country_covid_cases.sql
@@ -1,18 +1,3 @@
--- 这是一个 MySQL 方言的示例查询SQL
-select CAST(STR_TO_DATE(cases.date_rep, '%d/%m/%Y') AS DATE) as report_date, -- 报告日期
-       cases.cases                                           as cases, -- 病例数
-       cases.deaths                                          as deaths, -- 死亡数
-       country_codes.country                                 as country, -- 国家
-       cases.geo_id                                          as geo_id -- 地理标识
-from covid_cases as cases
-         join country_codes
-              on cases.geo_id = country_codes.alpha_2code
-{% if start_date -%}
-where CAST(STR_TO_DATE(cases.date_rep, '%d/%m/%Y') AS DATE) > '{{ start_date }}'
-{% endif %}
-;
-
-/*
 -- 这是一个 DuckDB 方言的示例查询SQL
 select CAST(strptime(cases.date_rep, '%d/%m/%Y') AS DATE)    as report_date, -- 报告日期
        cases.cases                                           as cases, -- 病例数
@@ -24,6 +9,21 @@ from covid_cases as cases
               on cases.geo_id = country_codes.alpha_2code
 {% if start_date -%}
 where CAST(strptime(cases.date_rep, '%d/%m/%Y') AS DATE) > '{{ start_date }}'
+{% endif %}
+;
+
+/*
+-- 这是一个 MySQL 方言的示例查询SQL
+select CAST(STR_TO_DATE(cases.date_rep, '%d/%m/%Y') AS DATE) as report_date, -- 报告日期
+       cases.cases                                           as cases, -- 病例数
+       cases.deaths                                          as deaths, -- 死亡数
+       country_codes.country                                 as country, -- 国家
+       cases.geo_id                                          as geo_id -- 地理标识
+from covid_cases as cases
+         join country_codes
+              on cases.geo_id = country_codes.alpha_2code
+{% if start_date -%}
+where CAST(STR_TO_DATE(cases.date_rep, '%d/%m/%Y') AS DATE) > '{{ start_date }}'
 {% endif %}
 ;
 */
