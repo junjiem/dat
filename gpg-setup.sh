a4a818909c5ac011b38dd74be0434bfe86c1eac3
Junjie.M <118170653@qq.com>
2025-10-29 18:05:04 +0800

    chore: release and deploy to maven central (#109)

---
+++ gpg-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Prepare GPG Key is expected to be in base64
+#
+# Linux/Mac:
+#   gpg -a --export-secret-keys "your@email" | base64 > gpg.base64
+#
+# Windows:
+#   1、gpg -a --export-secret-keys "your@email" > gpg.tmp
+#   2、certutil -encode gpg.tmp gpg.base64
+#
+printf "$GPG_KEY_BASE64" | base64 --decode > gpg.asc
+echo ${GPG_PASSPHRASE} | gpg --batch --yes --passphrase-fd 0 --import gpg.asc
+gpg -k
